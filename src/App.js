e1771ad0b683f9db65977426e56012f33ce16aaa
AnushkaK <AnushkaK@ipd.com.au>
2023-01-11 00:43:11 +0530

    Added Transactions

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
+import { BrowserRouter, Routes, Route , NavLink } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">
+          Expense Tracker
+        </h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/department">
+                Categories
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+                Transactions
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/department' element={<Department/>}></Route>
+          <Route path='/employee' element={<Employee/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
